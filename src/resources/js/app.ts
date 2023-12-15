9ccc5d2e493c1f0fdad9499f87a432567787b1d5
WEBzaytsev <studio@webzaytsev.ru>
2023-12-15 19:00:16 +0200

    feat: setup typescript

--- src/resources/js/app.ts
+++ src/resources/js/app.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from '@/App.vue';
-import router from '@/router/index.js';
-import store from '@/store/index.js';
+import router from '@/router';
+import store from '@/store';
 
 createApp(App).use(router).use(store).mount('#app');
