a44456b36d6127f53f4bd6d199f5bc17e37fd558
WEBZaytsev <nikita@zaitsv.dev>
2023-12-15 19:23:57 +0200

    refactor: remove unnecessary style input from config

--- src/vite.config.js
+++ src/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     plugins: [
         vue(),
         laravel({
-            input: ['resources/css/app.scss', 'resources/js/app.ts'],
+            input: ['resources/js/app.ts'],
             refresh: true,
         }),
     ],
