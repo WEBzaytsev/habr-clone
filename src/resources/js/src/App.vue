88a6079fe7619e8f867257ea87ef41b2948a106a
WEBZaytsev <nikita@zaitsv.dev>
2024-01-05 23:13:56 +0200

    edit base App

--- src/resources/js/src/App.vue
+++ src/resources/js/src/App.vue
@@ -1,8 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue';
+import AppSidebar from '@/components/AppSidebar.vue';
+import AppHeader from '@/components/AppHeader.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const isAuth = ref(false);
+
+watchEffect(setIsAuth);
+
+function setIsAuth(): void {
+    isAuth.value = route.name === 'login' || route.name === 'register';
+}
+</script>
 
 <template>
     <div class="max-h-screen h-screen">
-        <RouterView />
+        <div class="bg-black/50 flex justify-center items-center h-full" v-if="isAuth">
+            <RouterView />
+        </div>
+        <div class="grid grid-cols-[220px_auto] h-full" v-else>
+            <AppSidebar />
+            <div class="h-full">
+                <AppHeader title="Заголовок" />
+                <main class="p-7.5">
+                    <RouterView />
+                </main>
+            </div>
+        </div>
     </div>
 </template>
 
