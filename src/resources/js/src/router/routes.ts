36b3e546ea9b71333593bd7d6414bca4a0eda6c8
WEBZaytsev <nikita@zaitsv.dev>
2024-01-05 23:15:35 +0200

    fix login/register components paths

--- src/resources/js/src/router/routes.ts
+++ src/resources/js/src/router/routes.ts
@@ -1,6 +1,5 @@
-import HomeView from '@/views/HomeView.vue';
-import LoginView from '@/views/LoginView.vue';
-import RegisterView from '@/views/RigisterView.vue';
+import LoginView from '@/views/auth/LoginView.vue';
+import RegisterView from '@/views/auth/RigisterView.vue';
 import MainListView from '@/views/MainListView.vue';
 
 const routes = [
@@ -18,6 +17,28 @@ const routes = [
         path: '/',
         name: 'home',
         component: MainListView,
+    },
+    {
+        path: '/user',
+        name: 'user',
+        redirect: '/user/dashboard',
+        children: [
+            {
+                path: 'dashboard',
+                name: 'dashboard',
+                component: DashboardView,
+            },
+            {
+                path: 'my-trucks',
+                name: 'my-trucks',
+                component: MyTrucks,
+            },
+            {
+                path: 'my-cargos',
+                name: 'my-cargos',
+                component: MyCargos,
+            },
+        ],
     },
 ];
 
