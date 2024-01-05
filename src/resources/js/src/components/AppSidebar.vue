18eadc9e0b4dd13eeda2c3bef918ec474912504f
WEBZaytsev <nikita@zaitsv.dev>
2024-01-05 23:18:09 +0200

    add menu to sidebar

--- src/resources/js/src/components/AppSidebar.vue
+++ src/resources/js/src/components/AppSidebar.vue
@@ -1,4 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const menu = [
+    {
+        icon: 'dashboard',
+        text: 'Панель управления',
+        route: '/user/dashboard',
+    },
+    {
+        icon: 'local_shipping',
+        text: 'Мои машины',
+        route: '/user/my-trucks',
+    },
+    {
+        icon: 'package_2',
+        text: 'Мои грузы',
+        route: '/user/my-cargos',
+    },
+    {
+        icon: 'browse_activity',
+        text: 'Активные',
+        route: '#',
+    },
+    {
+        icon: 'chat',
+        text: 'Сообщения',
+        route: '#',
+    },
+    {
+        icon: 'history',
+        text: 'История',
+        route: '#',
+    },
+    {
+        icon: 'settings',
+        text: 'Настройки',
+        route: '#',
+    },
+];
+</script>
 
 <template>
     <aside class="h-full bg-emerald-400">
@@ -8,7 +46,22 @@
         >
             Logo
         </RouterLink>
-        <div class="p-3"></div>
+        <div class="flex flex-col justify-between h-[calc(100vh_-_71px)]">
+            <ul class="p-3 grid gap-2">
+                <li v-for="item in menu" :key="item.text">
+                    <RouterLink :to="item.route" class="flex items-center gap-2 text-white">
+                        <span class="material-symbols-outlined" v-text="item.icon" />
+                        <span v-text="item.text" />
+                    </RouterLink>
+                </li>
+            </ul>
+            <div class="p-3">
+                <RouterLink to="#" class="flex items-center gap-2 text-white">
+                    <span class="material-symbols-outlined" v-text="'logout'" />
+                    <span v-text="'Выйти'" />
+                </RouterLink>
+            </div>
+        </div>
     </aside>
 </template>
 
